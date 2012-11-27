a3855fdd391a524f206292335f784f7591ab3a19
Maxim Gladkov <maksim.gladkov@gmail.com>
2012-11-28 00:02:17 +0400

    t

---
+++ lib/rbkmoney/helper.rb
@@ -0,0 +1,50 @@
+module RBKMoney
+  module Helpers
+    def rbkmoney_form_tag(url = RBKMoney::purchase_uri, options = {})
+      form_tag(url, options)
+    end
+
+    def rbkmoney_form_tag(url = RBKMoney::purchase_uri, options = {}, &block)
+      if block
+        form_tag(url, options, &block)
+      else
+        form_tag(url, options)
+      end
+    end
+    
+    def rbkmoney_setup(options = {})
+      params = {
+        eshop_id: RBKMoney::eshop_id,
+        currency: RBKMoney::currency,
+        success_url: RBKMoney::success_url,
+        fail_url: RBKMoney::fail_url,
+        locale: 'ru',
+        custom: []
+      }.reject{|k,v| v.nil?}.merge(options)
+      
+      form = []
+      form << tag(:input, :type => 'hidden', :name => 'eshopId',            :value => params[:eshop_id])
+      form << tag(:input, :type => 'hidden', :name => 'orderId',            :value => params[:order_id])
+      form << tag(:input, :type => 'hidden', :name => 'serviceName',        :value => params[:item_name])
+      form << tag(:input, :type => 'hidden', :name => 'recipientAmount',    :value => params[:amount])
+      form << tag(:input, :type => 'hidden', :name => 'recipientCurrency',  :value => params[:currency])
+      form << tag(:input, :type => 'hidden', :name => 'successUrl',         :value => params[:success_url])
+      form << tag(:input, :type => 'hidden', :name => 'failUrl',            :value => params[:fail_url])
+      form << tag(:input, :type => 'hidden', :name => 'requestedLanguage',  :value => params[:locale])
+      form << tag(:input, :type => 'hidden', :name => 'userName',           :value => params[:user_name])
+      form << tag(:input, :type => 'hidden', :name => 'userEmail',          :value => params[:user_email])
+      
+      params[:custom].each_with_index do |value, i|
+        form << tag(:input, :type => 'hidden', :name => "userField_#{i}", :value => value)
+      end
+      
+      form.map(&:to_s).join("\n").html_safe
+    end
+  end
+
+  class Railtie < Rails::Railtie
+    initializer "rbkmoney.helpers" do
+      ActionView::Base.send :include, Helpers
+    end
+  end
+end
---
+++ lib/rbkmoney/notification.rb
@@ -0,0 +1,95 @@
+module RBKMoney
+  class Notification
+    attr_accessor :params
+
+    def initialize(p)
+      empty!
+      params = p
+    end
+
+    def accepted?
+      status == "3"
+    end
+
+    def completed?
+      status == "5"
+    end
+
+    def amount
+      recipient_amount.to_f
+    end
+    
+    def eshop_id
+      params['eshopId']
+    end
+    
+    def payment_id
+      params['paymentId']
+    end
+    
+    def order_id
+      params['orderId']
+    end
+    
+    def eshop_account
+      params['eshopAccount']
+    end
+    
+    def recipient_amount
+      params['recipientAmount']
+    end
+    
+    def currency
+      params['recipientCurrency']
+    end
+
+    def status
+      params['paymentStatus']
+    end
+    
+    def user_name
+      params['userName']
+    end
+    
+    def user_email
+      params['userEmail']
+    end
+
+    def payment_data
+      params['paymentData']
+    end
+
+    def received_at
+      Time.parse payment_data
+    end
+
+    def secret_key
+      params['secretKey']
+    end
+    
+    def hash
+      params['hash']
+    end
+    
+    def user_fields
+      params.select{ |p| p =~ /userField_[0-9]+/ }.map{ |k, v| v }
+    end
+    
+    def to_s
+      values = [:eshop_id, :order_id, :service_name, :recipient_amount, :currency, :status, :user_name, :user_email, :payment_data, :hash].map { |property| "#{property}: '#{self.send property}'" }.join(', ')
+      "<#{values}>"
+    end
+
+    def empty!
+      @params = Hash.new   
+    end
+    
+    def generated_hash
+      Digest::MD5.hexdigest([eshop_id, order_id, service_name, eshop_account, recipient_amount, currency, status, user_name, user_email, payment_data, secret_key].join('::'))
+    end
+
+    def acknowledge      
+      hash == generated_hash
+    end
+  end
+end
