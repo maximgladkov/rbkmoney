d9a6c9e1face2bdf2e2c88e82812f87451536c62
Dmitry <lutskov@mail.ru>
2017-11-08 00:41:56 +0300

    Update notification.rb

--- lib/rbkmoney/notification.rb
+++ lib/rbkmoney/notification.rb
@@ -90,7 +90,7 @@ module RBKMoney
                         @order.id,
                         RBKMoney::service_name,
                         RBKMoney::eshop_account,
-                        @order.total.to_s.gsub(".",",").to_f, 
+                        @order.total.to_s.gsub(".",","), 
                         currency,
                         status,
                         @order.email, 
